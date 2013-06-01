c89b3cc6498d2f2117f17b19ae2286b59118b7f9
Hunter Fuller <hfuller@iota.hackmiester.com>
2013-06-01 04:11:40 -0500

    initial commit to setup git

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>iGame</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<!--<link href="css/game.css" rel="stylesheet" media="screen">-->
+	<style type="text/css">
+		.btn-game {
+			margin-top:10px;
+			margin-bottom:10px;
+		}
+	</style>
+  </head>
+  <body>
+
+  <div class="container">
+	  <div class="row">
+			<div class="title text-center span6 offset3">
+				<h4>iGame</h4>
+				A port of Game by <a href="http://dr3v.com/">Nelson Gatlin</a>.
+			</div>
+			<div class="buttons span6 offset3">
+				<div class="row">
+					<button class="btn btn-mini btn-game span2" id="1"></button>
+					<button class="btn btn-mini btn-game span2" id="2"></button>
+					<button class="btn btn-mini btn-game span2" id="3"></button>
+				</div>
+				<div class="row">
+					<button class="btn btn-mini btn-game span2" id="4"></button>
+					<button class="btn btn-mini btn-game span2" id="5"></button>
+					<button class="btn btn-mini btn-game span2" id="6"></button>
+				</div>
+				<div class="row">
+					<button class="btn btn-mini btn-game span2" id="7"></button>
+					<button class="btn btn-mini btn-game span2" id="8"></button>
+					<button class="btn btn-mini btn-game span2" id="9"></button>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+	<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script>
+		var cw = $('.btn-game').width();
+		function toggle(theId) {
+			if ( theId >= 1 && theId <= 9 ) {
+				var theBtn = $('#' + theId);
+				if ( theBtn.hasClass('btn-danger') ) {
+					theBtn.removeClass('btn-danger');
+				} else {
+					theBtn.addClass('btn-danger');
+				}	
+			}
+		}
+		$('.btn-game')
+			.css({'height':cw+'px'})
+			.click(function(){
+				
+				//do the toggles
+				theId = parseInt($(this).attr("id"));
+				toggle(theId);						//toggle myself
+				toggle(theId-3); toggle(theId+3);	//toggle above and below me
+				if ( theId%3 != 1 ) { //if I'm not the leftmost
+					toggle(theId-1); //toggle to the left of me
+				}
+				if ( theId%3 != 0 ) { //if I'm not the rightmost
+					toggle(theId+1); //toggle to the right of me
+				}
+				
+				//check if the user won this round
+				if ( $('.btn-danger').length >= 9 ) {
+					alert("Great job dickhead");
+					$('.btn-danger').removeClass('btn-danger'); //reset field to white
+					//set the field based on the winning click
+					switch ( theId ) {
+						case 1:
+							$('#2').addClass('btn-danger');
+							$('#4').addClass('btn-danger');
+							$('#8').addClass('btn-danger');
+							break;
+						case 2:
+							$('#5').addClass('btn-danger');
+							$('#8').addClass('btn-danger');
+							break;
+						case 3:
+						case 6:
+							$('#1').addClass('btn-danger');
+							$('#6').addClass('btn-danger');
+							$('#9').addClass('btn-danger');
+							break;
+						case 4:
+							$('#2').addClass('btn-danger');
+							$('#5').addClass('btn-danger');
+							$('#7').addClass('btn-danger');
+							break;
+						case 5:
+							$('#2').addClass('btn-danger');
+							$('#7').addClass('btn-danger');
+							$('#9').addClass('btn-danger');
+							break;
+						case 7:
+							$('#1').addClass('btn-danger');
+							$('#3').addClass('btn-danger');
+							$('#8').addClass('btn-danger');
+							break;
+						case 8:
+							$('#2').addClass('btn-danger');
+							$('#3').addClass('btn-danger');
+							$('#5').addClass('btn-danger');
+							$('#8').addClass('btn-danger');
+							break;
+						case 9:
+							$('#2').addClass('btn-danger');
+							$('#5').addClass('btn-danger');
+							$('#7').addClass('btn-danger');
+							$('#9').addClass('btn-danger');
+							break;
+					}
+				}
+				
+			})
+		;
+	</script>
+
+  </body>
+</html>
